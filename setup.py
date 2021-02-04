d2767fd737690abf475b35149dc05662077cca97
Sushmit Roy <sushmit86@gmail.com>
2021-02-04 17:58:01 -0500

    adding

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ requirements = [
 ]
 
 setup(
-    name='sushutil',
+    name='oishani',
     version='0.1.0',
     python_requires='>=3.5',
     author='sushmit roy',
     author_email='sushmit86@gmail.com',
     description='sushmit util',
-    packages=find_packages(),
+    packages=find_packages(include=['myutil']),
     zip_safe=True,
     install_requires=requirements,
 )
